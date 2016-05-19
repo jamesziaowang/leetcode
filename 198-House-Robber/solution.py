a70f09831d2ed17a42f148b49eb6095dfac4196c
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-19 16:29:57 +1000

    [198. House Robber][Accepted]committed by LeetCode Extension

--- 198-House-Robber/solution.py
+++ 198-House-Robber/solution.py
@@ -1,12 +1,9 @@
 class Solution(object):
     def rob(self, nums):
-        n = len(nums)
-        if n == 0:
-            return 0
-        elif n == 1:
-            return nums[0]
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2,n):
-            dp[i] = max(nums[i-1], nums[i-2]+nums[i])
-        return dp
+        size = len(nums)
+        dp = [0] * (size + 1)
+        if size:
+            dp[1] = nums[0]
+        for i in range(2, size + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+        return dp[size]
