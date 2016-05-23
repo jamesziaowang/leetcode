d71e55c57783055800849b5d0ef060e073fc96e8
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-23 11:57:14 +1000

    [107. Binary Tree Level Order Traversal II][Accepted]committed by LeetCode Extension

--- 107-Binary-Tree-Level-Order-Traversal-II/solution.py
+++ 107-Binary-Tree-Level-Order-Traversal-II/solution.py
@@ -10,17 +10,17 @@ class Solution(object):
         if not root:
             return []
         queue = []
-        queue.append(root)
-        level = []
         ret = []
+        queue.append(root)
         while queue:
             n = len(queue)
-            for i in range(0,n):
+            level = []
+            for i in range(n):
                 temp = queue.pop(0)
                 level.append(temp.val)
                 if temp.left:
                     queue.append(temp.left)
-                elif temp.right:
+                if temp.right:
                     queue.append(temp.right)
             ret.insert(0,level)
         return ret
