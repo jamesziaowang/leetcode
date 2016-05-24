3eb5a6ecd1b3ec446c341b1ebc59001ca3317fb9
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-25 00:42:55 +1000

    [111. Minimum Depth of Binary Tree][Runtime Error]committed by LeetCode Extension

---
+++ 111-Minimum-Depth-of-Binary-Tree/solution.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        if not root:
+            return 0
+        minLeft = self.minDepth(root.left)
+        minRight = self.minDepth(root.right)
+        if minleft == 0 or minRight == 0:
+            return minLeft+1  if minLeft>=minRight else minRight +1
+        return min(minLeft,minRight)+1
