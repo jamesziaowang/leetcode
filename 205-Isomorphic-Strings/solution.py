626ca146a0c1e035a97e19e581b12f814ea18071
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-26 16:30:10 +1000

    [205. Isomorphic Strings][Runtime Error]committed by LeetCode Extension

---
+++ 205-Isomorphic-Strings/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        sDict = dict()
+        slen = len(s)
+        tlen = len(t)
+        if slen != tlen:
+            return False
+        for i in range(slen):
+            tempS = sDict.get(s[i])
+            if tempS and tempS != t[i]:
+                    return False
+            tempS[s[i]] = t[i]
+        return True
+            
+            
