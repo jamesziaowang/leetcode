7bbed74ac2829a5857da170e90deea3aa798f449
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-27 12:26:45 +1000

    [257. Binary Tree Paths][Runtime Error]committed by LeetCode Extension

---
+++ 257-Binary-Tree-Paths/solution.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+    
+    def help(self, nodeList, node):
+        if node: nodeList.append(node.val)
+        if node.left:
+            self.help(nodeList,node.left)
+        elif node.right:
+            self.help(nodeList,node.right)
+        else:
+            return nodeList
+    
+    def binaryTreePaths(self, root):
+        if not root:
+            return []
+        ret = []
+        l = []
+        r = []
+        l.append(root.val)
+        r.append(root.val)
+        ret.append()
+        if root.left: ret.append(self.help(l,root.left))
+        if root.right:ret.append(self.help(r,root.right))
+        return ret
+        
