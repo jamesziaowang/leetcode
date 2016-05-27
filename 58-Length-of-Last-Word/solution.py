1be5f551f99ed47a53b6e7d4b4f49d67408b5484
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-27 11:59:22 +1000

    [58. Length of Last Word][Runtime Error]committed by LeetCode Extension

---
+++ 58-Length-of-Last-Word/solution.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.split()[len(s.split()-1)]) if s.split() != [] else 0 
