9fd543fb23bedba51fb49471f702883fd9a501e8
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-30 13:35:48 +1000

    [38. Count and Say][Runtime Error]committed by LeetCode Extension

--- 38-Count-and-Say/solution.py
+++ 38-Count-and-Say/solution.py
@@ -8,8 +8,8 @@ class Solution(object):
         for i in range(n-1):
             count=1
             curStr = ""
-            for j in range(len(oldstr)+1):
-                if j<len(oldstr) and oldstr[j]==oldstr[j+1]:
+            for j in range(1,len(oldstr)+1):
+                if j<len(oldstr) and oldstr[j]==oldstr[j-1]:
                     count+=1
                 else:
                     curStr = curStr + str(count)+oldstr[j]
