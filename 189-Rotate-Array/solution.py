e53952f4a9243120c058bc99906dfc643ad2da2a
jamesziaowang <james.ziao.wang@gmail.com>
2016-05-31 12:58:12 +1000

    [189. Rotate Array][Runtime Error]committed by LeetCode Extension

--- 189-Rotate-Array/solution.py
+++ 189-Rotate-Array/solution.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def reverseNums(self, nums, start, end):
-        for i in range(start, (start+end)/2):
+        for i in range(start, (end-start)/2):
             nums[i] ^= nums[end-i-1] 
             nums[end-i-1] ^= nums[i] 
             nums[i] ^= nums[end-i-1] 
