5bb7b630070c455bd058de4c98a934391f3482cc
jamesziaowang <james.ziao.wang@gmail.com>
2016-06-02 11:32:54 +1000

    [14. Longest Common Prefix][Runtime Error]committed by LeetCode Extension

--- 14-Longest-Common-Prefix/solution.py
+++ 14-Longest-Common-Prefix/solution.py
@@ -10,6 +10,6 @@ class Solution(object):
         for i in range(1,len(strs)):
             for j in range(len(strs[i])):
                 if j>len(pre) or pre[j] != strs[i][j]:
-                    pre = pre.substr(0,j)
+                    pre = pre[0:j]
                     break;
         return pre
