450792f9ea45baa410ccc9f3370c1bca6471c63e
jamesziaowang <james.ziao.wang@gmail.com>
2016-06-06 13:41:37 +1000

    [349. Intersection of Two Arrays][Runtime Error]committed by LeetCode Extension

--- 349-Intersection-of-Two-Arrays/solution.py
+++ 349-Intersection-of-Two-Arrays/solution.py
@@ -8,7 +8,7 @@ class Solution(object):
         nums1,nums2 = sorted(nums1),sorted(nums2)
         p1 = p2 = 0
         ret = set()
-        while p1<len(nums1)and p2 < len(nums2)
+        while p1<len(nums1) and p2 < len(nums2):
             if nums1[p1] > nums2[p2]:
                 p2+=1
             elif nums1[p1] < nums2[p2]:
