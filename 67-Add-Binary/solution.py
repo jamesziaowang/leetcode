d8c6f84d5f77f7975091b03341de78ca89ab71bd
jamesziaowang <james.ziao.wang@gmail.com>
2016-06-09 10:29:12 +1000

    [67. Add Binary][Runtime Error]committed by LeetCode Extension

--- 67-Add-Binary/solution.py
+++ 67-Add-Binary/solution.py
@@ -5,10 +5,20 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        i,j = len(a),len(b)
-        while i>=0 and j>=0:
-            c += int(a[i-=1]) if i>=0 else 0
-            c += int(a[j-=1]) if j>=0 else 0
+        i,j = len(a)-1,len(b)-1
+        while i>=0 or j>=0:
+            if i>=0:
+                c += int(a[i]) 
+                i-=1
+            else:
+                c += 0
+                
+            if j>=0:
+                c += int(a[j]) 
+                j-=1
+            else:
+                c += 0
+                
             s = str(c % 2) + s
             c /= 2
         return s
