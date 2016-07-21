f6a0c82c1f4ff5c6fa160c35ec104a503cb72395
jamesziaowang <james.ziao.wang@gmail.com>
2016-07-21 20:00:33 +1000

    [242. Valid Anagram][Accepted]committed by LeetCode Extension

---
+++ 242-Valid-Anagram/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        s = sorted(s)
+        t = sorted(t)
+        
+        if s == t:
+            return True
+        else:
+            return False
+            
+        
