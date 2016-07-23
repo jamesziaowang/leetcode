1cb9294864aad4d68cf9f12450ee9cdafb267b62
jamesziaowang <james.ziao.wang@gmail.com>
2016-07-23 18:07:20 +1000

    [127. Word Ladder][Compile Error]committed by LeetCode Extension

---
+++ 127-Word-Ladder/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if(wordList.length<=0) return 0;
+        List<Note> queue = new Linkedlist<Node>();
+        queue.push(new Node(beginWord,1));
+        while(!queue.isEmpty()){
+            Node top = queue.pop();
+            if(top.word.equals(endWord)) return top.len;
+            
+            for(int i=0; i<top.word.length();i++){
+                Char[] charArray = top.word.toCharArray();
+                for(char c ='a'; c<='z';c++){
+                    charArray[i] = c;
+                    String newWord = new String(charArray);
+                    if(wordList.contaits(newWord)){
+                        queue.push(new Node(newWord,top.word.len+1));
+                        newWord.remove(newWord);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
